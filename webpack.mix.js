400f50ae7856f305e2663896c375d0690d7fc64a
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2020-11-14 20:59:09 +0100

    Installed postcss-nesting PostCSS plugin; some CSS rewriting/style changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss')
+    .postCss('resources/css/app.pcss', 'public/css', [
+        require('tailwindcss'),
+        require('postcss-nesting'),
     ]);
 
 if(mix.inProduction()) {
