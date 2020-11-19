7cc7ce545af6445511e8d933b5502aa4ec50967c
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2020-11-19 22:11:04 +0100

    Moved user seeding and author seeding into separate seeder classes

---
+++ database/seeders/AuthorSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Author;
+use App\Models\Line;
+use App\Models\Poem;
+use App\Models\Stanza;
+use Illuminate\Database\Seeder;
+
+class AuthorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Author::factory()
+            ->count(25)
+            ->has(
+                Poem::factory()
+                    ->count(rand(5, 15))
+                    ->has(
+                        Stanza::factory()
+                            ->count(rand(1, 5))
+                            ->has(
+                                Line::factory()
+                                    ->count(rand(2, 6))
+                            )
+                    )
+            )
+            ->create();
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Author;
-use App\Models\Line;
-use App\Models\Poem;
-use App\Models\Stanza;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,26 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()
-            ->state([
-                'email' => 'webmaster@localhost',
-            ])
-            ->create();
-
-        Author::factory()
-            ->count(25)
-            ->has(
-                Poem::factory()
-                    ->count(rand(5, 15))
-                    ->has(
-                        Stanza::factory()
-                            ->count(rand(1, 5))
-                            ->has(
-                                Line::factory()
-                                    ->count(rand(2, 6))
-                            )
-                    )
-            )
-            ->create();
+        $this->call([
+            UserSeeder::class,
+            AuthorSeeder::class,
+        ]);
     }
 }
