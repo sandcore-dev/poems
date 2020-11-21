785da2fa89eedd2f23b17c232cc12fcd3b220084
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2020-11-21 14:16:47 +0100

    Always show a different poem on every reload until they all have been shown

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Poem;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(): Renderable
+    public function index(Request $request): Renderable
     {
+        $usedPoemIds = $request->session()->get('used_poem_ids', []);
+        if (count($usedPoemIds) >= Poem::count()) {
+            $usedPoemIds = [];
+        }
+
         /** @var Poem $poem */
         $poem = Poem::with(['author', 'stanzas.lines'])
+            ->whereNotIn('id', $usedPoemIds)
             ->inRandomOrder()
             ->first();
+
+        $usedPoemIds[] = $poem->id;
+        $request->session()->put('used_poem_ids', $usedPoemIds);
 
         return view('home')
             ->with([
